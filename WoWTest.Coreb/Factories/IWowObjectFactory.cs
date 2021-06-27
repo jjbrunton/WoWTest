e828e03f21b965d9e07087a8f76dcf2206a21aac
Jamie Brunton <jamie@jbrunton.co.uk>
2021-06-27 15:06:19 +0100

    Remove old core project

--- WoWTest.Coreb/Factories/IWowObjectFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using WoWTestConsole.Models;
-
-namespace WoWTestConsole.Factories
-{
-    public interface IWowObjectFactory
-    {
-        T Create<T>(WowObject wowObject) where T : WowObject;
-    }
-}
